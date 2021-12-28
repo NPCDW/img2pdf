c2fb5fbc8ddd2e95da016ff8baa5ddd6327fccab
chenyongli <chenyongli0520@qq.com>
2021-12-28 21:26:41 +0800

    预览pdf

--- src/components/ImgToPdf.vue
+++ src/components/ImgToPdf.vue
@@ -1,110 +1,115 @@
 <template>
-  <div style="margin: 40px;">
-    <a-upload
-        action="#"
-        list-type="picture-card"
-        accept="image/*"
-        :file-list="fileList"
-        :before-upload="beforeUpload"
-        @preview="handlePreview"
-        @change="handleChange"
-        :multiple="true"
-    >
-      <a-icon type="plus"/>
-      <div class="ant-upload-text">
-        Upload
-      </div>
-    </a-upload>
-    <a-modal v-model="previewVisible" :footer="null" :closable="false">
-      <img alt="example" style="width: 100%" :src="previewImage"/>
-    </a-modal>
-    <div style="margin-top: 40px">
-      <div>
-        <span style="margin-right: 10px">计量单位：</span>
-        <a-radio-group v-model="unit" :disabled="unitDisabled">
-          <a-radio value="pt">points</a-radio>
-          <a-radio value="mm">mm</a-radio>
-          <a-radio value="cm">cm</a-radio>
-          <a-radio value="in">in</a-radio>
-          <a-radio value="px">px</a-radio>
-          <a-radio value="pc">pc</a-radio>
-          <a-radio value="em">em</a-radio>
-          <a-radio value="ex">ex</a-radio>
-        </a-radio-group>
-      </div>
-      <div style="margin-top: 20px">
-        <span style="margin-right: 10px">纸张大小：</span>
-        <a-radio-group v-model="pageSizeRadio" @change="pageSizeChange">
-          <a-radio :value="0">a4</a-radio>
-          <a-radio :value="1">b3</a-radio>
-          <a-radio :value="2">原图尺寸</a-radio>
-          <a-radio :value="3">自定义规格</a-radio>
-          <a-radio :value="4">自定义尺寸</a-radio>
-        </a-radio-group>
-      </div>
-      <div v-if="pageSizeRadio === 3" style="margin-top: 20px">
-        <ul>
-          <li>a0 - a10</li>
-          <li>b0 - b10</li>
-          <li>c0 - c10</li>
-          <li>dl</li>
-          <li>letter</li>
-          <li>government-letter</li>
-          <li>legal</li>
-          <li>junior-legal</li>
-          <li>ledger</li>
-          <li>tabloid</li>
-          <li>credit-card</li>
-        </ul>
-        <a-input v-model="pageSize" style="width: 400px" placeholder="请输入上面列出来的值，如：a4、b3" />
-      </div>
-      <div v-if="pageSizeRadio === 4" style="margin-top: 20px">
-        <a-input-number v-model="pageWidth" style="width: 200px" placeholder="如：210" />
-        <span> × </span>
-        <a-input-number v-model="pageHeight" style="width: 200px" placeholder="如：297" />
-      </div>
-      <div style="margin-top: 20px">
+  <div style="display: flex;margin: 40px;">
+    <div style="height: 800px;min-width:550px;position:relative; z-index:999">
+      <embed :src="pdfSrc" type="application/pdf" style="overflow: auto; width: 100%; height: 100%;"/>
+    </div>
+    <div style="margin: 40px;">
+      <a-upload
+          action="#"
+          list-type="picture-card"
+          accept="image/*"
+          :file-list="fileList"
+          :before-upload="beforeUpload"
+          @preview="handlePreview"
+          @change="handleChange"
+          :multiple="true"
+      >
+        <a-icon type="plus"/>
+        <div class="ant-upload-text">
+          Upload
+        </div>
+      </a-upload>
+      <a-modal v-model="previewVisible" :footer="null" :closable="false">
+        <img alt="example" style="width: 100%" :src="previewImage"/>
+      </a-modal>
+      <div style="margin-top: 40px">
         <div>
-          <span style="margin-right: 10px">纸张方向：</span>
-          <a-radio-group v-model="pageDirection" :disabled="pageDirectionDisabled">
-            <a-radio value="portrait">纵向</a-radio>
-            <a-radio value="landscape">横向</a-radio>
+          <span style="margin-right: 10px">计量单位：</span>
+          <a-radio-group v-model="unit" :disabled="unitDisabled">
+            <a-radio value="pt">points</a-radio>
+            <a-radio value="mm">mm</a-radio>
+            <a-radio value="cm">cm</a-radio>
+            <a-radio value="in">in</a-radio>
+            <a-radio value="px">px</a-radio>
+            <a-radio value="pc">pc</a-radio>
+            <a-radio value="em">em</a-radio>
+            <a-radio value="ex">ex</a-radio>
           </a-radio-group>
         </div>
-      </div>
-      <div style="margin-top: 20px">
-        <div>
-          <span style="margin-right: 10px">左右边距：</span>
-          <a-input-number v-model="x" :min="0" />
+        <div style="margin-top: 20px">
+          <span style="margin-right: 10px">纸张大小：</span>
+          <a-radio-group v-model="pageSizeRadio" @change="pageSizeChange">
+            <a-radio :value="0">a4</a-radio>
+            <a-radio :value="1">b3</a-radio>
+            <a-radio :value="2">原图尺寸</a-radio>
+            <a-radio :value="3">自定义规格</a-radio>
+            <a-radio :value="4">自定义尺寸</a-radio>
+          </a-radio-group>
         </div>
-      </div>
-      <div style="margin-top: 20px">
-        <div>
-          <span style="margin-right: 10px">上下边距：</span>
-          <a-input-number v-model="y" :min="0" />
+        <div v-if="pageSizeRadio === 3" style="margin-top: 20px">
+          <ul>
+            <li>a0 - a10</li>
+            <li>b0 - b10</li>
+            <li>c0 - c10</li>
+            <li>dl</li>
+            <li>letter</li>
+            <li>government-letter</li>
+            <li>legal</li>
+            <li>junior-legal</li>
+            <li>ledger</li>
+            <li>tabloid</li>
+            <li>credit-card</li>
+          </ul>
+          <a-input v-model="pageSize" style="width: 400px" placeholder="请输入上面列出来的值，如：a4、b3"/>
         </div>
-      </div>
-      <div style="margin-top: 20px">
-        <div>
-          <span style="margin-right: 10px">旋转角度：</span>
-          <a-input-number v-model="rotation" :min="0" :max="359" />
+        <div v-if="pageSizeRadio === 4" style="margin-top: 20px">
+          <a-input-number v-model="pageWidth" style="width: 200px" placeholder="如：210"/>
+          <span> × </span>
+          <a-input-number v-model="pageHeight" style="width: 200px" placeholder="如：297"/>
         </div>
-      </div>
-      <div style="margin-top: 20px">
-        <div>
-          <span style="margin-right: 10px">访问密码：</span>
-          <a-input style="width: 400px" v-model="password" />
+        <div style="margin-top: 20px">
+          <div>
+            <span style="margin-right: 10px">纸张方向：</span>
+            <a-radio-group v-model="pageDirection" :disabled="pageDirectionDisabled">
+              <a-radio value="portrait">纵向</a-radio>
+              <a-radio value="landscape">横向</a-radio>
+            </a-radio-group>
+          </div>
         </div>
-      </div>
-      <div style="margin-top: 20px;">
-        <a-button type="primary" @click="convert" :loading="loading">导出PDF</a-button>
+        <div style="margin-top: 20px">
+          <div>
+            <span style="margin-right: 10px">左右边距：</span>
+            <a-input-number v-model="x" :min="0"/>
+          </div>
+        </div>
+        <div style="margin-top: 20px">
+          <div>
+            <span style="margin-right: 10px">上下边距：</span>
+            <a-input-number v-model="y" :min="0"/>
+          </div>
+        </div>
+        <div style="margin-top: 20px">
+          <div>
+            <span style="margin-right: 10px">旋转角度：</span>
+            <a-input-number v-model="rotation" :min="0" :max="359"/>
+          </div>
+        </div>
+        <div style="margin-top: 20px">
+          <div>
+            <span style="margin-right: 10px">访问密码：</span>
+            <a-input style="width: 400px" v-model="password"/>
+          </div>
+        </div>
+        <div style="margin-top: 20px;">
+          <a-button type="primary" @click="convert" :loading="loading">导出PDF</a-button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { jsPDF } from "jspdf";
+import {jsPDF} from "jspdf";
 
 export default {
   name: "ImgToPdf",
@@ -126,6 +131,7 @@ export default {
       y: 0,
       unit: 'mm',
       password: null,
+      pdfSrc: null,
     };
   },
   methods: {
@@ -151,31 +157,30 @@ export default {
             userPassword: this.password ? this.password : null,
           }
         });
-        let zoom = Math.max(file0.width/(doc.getPageWidth(1) - (this.x * 2)),file0.height/(doc.getPageHeight(1) - (this.y * 2)))
+        let zoom = Math.max(file0.width / (doc.getPageWidth(1) - (this.x * 2)), file0.height / (doc.getPageHeight(1) - (this.y * 2)))
         doc.addImage({
           imageData: file0.preview,
           x: this.x,
           y: this.y,
-          width: file0.width/zoom,
-          height: file0.height/zoom,
+          width: file0.width / zoom,
+          height: file0.height / zoom,
           rotation: this.rotation,
         })
         for (let i = 1; i < this.fileList.length; i++) {
           let fileTmp = this.fileList[i]
           doc.addPage(fileTmp.pageSize, fileTmp.pageDirection);
-          let zoom = Math.max(fileTmp.width/(doc.getPageWidth(i+1) - (this.x * 2)),fileTmp.height/(doc.getPageHeight(i+1) - (this.y * 2)))
+          let zoom = Math.max(fileTmp.width / (doc.getPageWidth(i + 1) - (this.x * 2)), fileTmp.height / (doc.getPageHeight(i + 1) - (this.y * 2)))
           doc.addImage({
             imageData: fileTmp.preview,
             x: this.x,
             y: this.y,
-            width: fileTmp.width/zoom,
-            height: fileTmp.height/zoom,
+            width: fileTmp.width / zoom,
+            height: fileTmp.height / zoom,
             rotation: this.rotation,
           })
         }
-        doc.save('test.pdf', {returnPromise: true}).finally(() => {
-          this.loading = false
-        })
+        this.pdfSrc = doc.output("bloburi")
+        this.loading = false
       }).catch(() => {
         this.$message.error("导出失败")
       })
